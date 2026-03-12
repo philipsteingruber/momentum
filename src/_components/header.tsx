8e1b20d81a3665c0f6cc65e35e1b24f08814c018
Philip Steingrüber <philip.steingruber@gmail.com>
2026-03-12 11:20:40 +0100

    feat(header.tsx): add dropdown menu for navigation links in the header to enhance user experience and accessibility

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -1,12 +1,37 @@
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4 text-lg w-full">
+    <header className="flex items-center justify-between py-4 pr-4 pl-8 text-lg w-full border-b-2">
       <span className="font-semibold">MOMENTUM</span>
       <Show when={"signed-in"}>
-        <UserButton />
+        <div className="flex items-center gap-x-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant={"outline"}>Go to...</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <Link href={"/tasks"}>
+                <DropdownMenuItem>Tasks</DropdownMenuItem>
+              </Link>
+              <Link href={"/categories"}>
+                <DropdownMenuItem>Categories</DropdownMenuItem>
+              </Link>
+              <Link href={"/tags"}>
+                <DropdownMenuItem>Tags</DropdownMenuItem>
+              </Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <UserButton />
+        </div>
       </Show>
       <Show when={"signed-out"}>
         <div className="flex items-center gap-x-4">
